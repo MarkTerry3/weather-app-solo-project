aa0674d6b11ee39188586a3f03fbd6863953daac
MarkTerry3 <mark.terry93@gmail.com>
2022-03-18 10:39:24 -0500

    edit and delete pop ups done and working

--- src/components/AccountPage/AccountPage.jsx
+++ src/components/AccountPage/AccountPage.jsx
@@ -2,11 +2,18 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import * as React from 'react';
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import {useState} from 'react';
+import { useState } from 'react';
 
 
 function AccountPage() {
@@ -27,11 +34,11 @@ function AccountPage() {
 
         dispatch({
             type: 'EDIT_ZIP_CODE',
-            payload: {user_id, zipCode}
+            payload: { user_id, zipCode }
         })
         // setZipCode('');
         history.push('/home');
-        
+
     }
 
     // pass the user to the saga so we know who to delete.
@@ -47,15 +54,15 @@ function AccountPage() {
         history.push('/login');
     }
 
-    // mui stuff you need for the modal
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    // // mui stuff you need for the modal
+    // const [open, setOpen] = React.useState(false);
+    // const handleOpen = () => setOpen(true);
+    // const handleClose = () => setOpen(false);
 
-    // mui styling you need
-    const style = {
+    // // mui styling you need
+    const styleEdit = {
         position: 'absolute',
-        top: '50%',
+        top: '25%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
         width: 400,
@@ -63,38 +70,167 @@ function AccountPage() {
         border: '2px solid #000',
         boxShadow: 24,
         p: 4,
-      };
+    };
+
+    const styleDelete = {
+        position: 'absolute',
+        top: '35%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+        backgroundColor: "red"
+    };
+
+    // return (
+    //     <>
+    //         <Button style={{ backgroundColor: "teal" }} variant="contained" onClick={handleOpen}>Edit Zip Code</Button>
+    //         <br />
+    //         <hr />
+    //         <Button style={{ backgroundColor: "red" }} variant="contained" onClick={deleteAccount}>Delete Account</Button>
+
+    //         <Modal
+    //             open={open}
+    //             onClose={handleClose}
+    //             aria-labelledby="modal-modal-title"
+    //             aria-describedby="modal-modal-description"
+    //         >
+    //             <Box sx={style}>
+    //                 <Typography id="modal-modal-title" variant="h6" component="h2">
+    //                     Edit Zip Code
+    //                 </Typography>
+    //                     <TextField 
+    //                         sx={{ mt: 2}}
+    //                         required
+    //                         id="outlined-zipcode-input"
+    //                         label="Zip Code"
+    //                         value={zipCode}
+    //                         onChange={(event) => setZipCode(event.target.value)}
+    //                     />
+    //                     <Button style={{ backgroundColor: "teal" }} variant="contained" onClick={editAccount}>Submit</Button>
+    //             </Box>
+    //         </Modal>
+    //     </>
+    // )
+
+    // ============================================================================================================================
+    // Stuff for Edit below
+    // ============================================================================================================================
+
+    const [openEdit, setOpenEdit] = React.useState(false);
+
+    const handleClickOpenEdit = () => {
+        setOpenEdit(true);
+    };
+
+    const handleCloseEdit = () => {
+        setOpenEdit(false);
+    };
+
+
+    // ============================================================================================================================
+    // Stuff for Delete below
+    // ============================================================================================================================
+
+
+    // const [openDelete, setOpenDelete] = React.useState(false);
+
+    // const handleClickOpenDelete = () => {
+    //     setOpenDelete(true);
+    // };
+
+    // const handleCloseDelete = () => {
+    //     setOpenDelete(false);
+    // };
+
+
+
+
+
+    const [openDelete, setOpenDelete] = React.useState(false);
+
+
+    const handleClickOpenDelete = () => {
+        setOpenDelete(true);
+    };
+
+    const handleCloseDelete = () => {
+        setOpenDelete(false);
+    };
+
+
+
+
 
     return (
-        <>
-            <Button style={{ backgroundColor: "teal" }} variant="contained" onClick={handleOpen}>Edit Zip Code</Button>
-            <br />
-            <hr />
-            <Button style={{ backgroundColor: "red" }} variant="contained" onClick={deleteAccount}>Delete Account</Button>
-
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Edit Zip Code
-                    </Typography>
-                        <TextField 
-                            sx={{ mt: 2}}
-                            required
+        <div>
+            <Button style={styleEdit} variant="contained" onClick={handleClickOpenEdit}>Edit Zip Code</Button>
+            <Button style={styleDelete} variant="contained" onClick={handleClickOpenDelete}>Delete Account</Button>
+            <Dialog open={openEdit} onClose={handleCloseEdit}>
+                <DialogTitle>Edit Zip Code</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="outlined-zipcode-input"
+                        label="New Zip Code"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={zipCode}
+                        onChange={(event) => setZipCode(event.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseEdit}>Cancel</Button>
+                    <Button onClick={editAccount}>Submit</Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog open={openDelete} onClose={handleCloseEdit}>
+                {/* <DialogTitle>Edit Zip Code</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
                             id="outlined-zipcode-input"
-                            label="Zip Code"
+                            label="New Zip Code"
+                            type="text"
+                            fullWidth
+                            variant="standard"
                             value={zipCode}
                             onChange={(event) => setZipCode(event.target.value)}
                         />
-                        <Button style={{ backgroundColor: "teal" }} variant="contained" onClick={editAccount}>Submit</Button>
-                </Box>
-            </Modal>
-        </>
-    )
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseEdit}>Cancel</Button>
+                        <Button onClick={editAccount}>Submit</Button>
+                    </DialogActions> */}
+            </Dialog>
+            <Dialog
+                open={openDelete}
+                onClose={handleCloseDelete}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Delete Account?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Are you sure you want to Delete your account? This action is irreversable.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDelete}>Cancel</Button>
+                    <Button onClick={deleteAccount}>DELETE ACCOUNT</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+
+
 }
+
 
 export default AccountPage;
