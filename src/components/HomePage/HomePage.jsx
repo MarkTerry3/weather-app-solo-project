c27c1eeb46f37a0bc777005b9f7ca0af7423337b
MarkTerry3 <mark.terry93@gmail.com>
2022-03-18 08:29:44 -0500

    radar working on page, need to create reducers to pass it values still

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -11,6 +11,23 @@ import './HomePage.css'
 
 function InfoPage() {
 
+//   var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+//     /* No need to set credentials if already passed in URL */
+//     center: new Microsoft.Maps.Location(39.1887643719098, -92.8261546188403),
+//     zoom: 5
+// });
+// // tile url from Iowa Environmental Mesonet of Iowa State University
+// var urlTemplate = 'https://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-{timestamp}/{zoom}/{x}/{y}.png';
+// var timestamps = ['900913-m50m', '900913-m45m', '900913-m40m', '900913-m35m', '900913-m30m', '900913-m25m', '900913-m20m', '900913-m15m', '900913-m10m', '900913-m05m', '900913'];
+// var tileSources = [];
+// for (var i = 0; i < timestamps.length; i++) {
+//     var tileSource = new Microsoft.Maps.TileSource({
+//         uriConstructor: urlTemplate.replace('{timestamp}', timestamps[i])
+//     });
+//     tileSources.push(tileSource);
+// }
+// var animatedLayer = new Microsoft.Maps.AnimatedTileLayer({ mercator: tileSources, frameRate: 500 });
+// map.layers.insert(animatedLayer);
 
   useEffect(() => {
     // dispatch({
@@ -53,8 +70,12 @@ const fetchWeather = () => {
       <Button style={{backgroundColor: "teal"}} variant="contained" onClick={fetchWeather}>Current Weather</Button>
       <p>Current Temperature in Saint Paul, MN :  {temperature} and {weatherText}</p>
       <progress className='progressBar' value={temperature} max='100'></progress>
+      <iframe width="650" height="450" src="https://embed.windy.com/embed2.html?lat=34.0522&lon=-118.2437&detailLat=44.784&detailLon=-93.220&width=650&height=450&zoom=10&level=surface&overlay=wind&product=ecmwf&menu=&message=&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1" frameborder="0"></iframe>
     </div>
   );
 }
 
 export default InfoPage;
+
+
+
