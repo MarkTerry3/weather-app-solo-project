d4ea8d964031feb053b05cbe5ffdbfee20ab3150
MarkTerry3 <mark.terry93@gmail.com>
2022-03-18 09:25:31 -0500

    REVERT BACK TO HERE, radar resized, going to work on delete account confirm pop up

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -71,7 +71,7 @@ const fetchWeather = () => {
       <Button style={{backgroundColor: "teal"}} variant="contained" onClick={fetchWeather}>Current Weather</Button>
       <p>Current Temperature in Saint Paul, MN :  {temperature} and {weatherText}</p>
       <progress className='progressBar' value={temperature} max='100'></progress>
-      <iframe width="650" height="450" src="https://embed.windy.com/embed2.html?lat=44.949&lon=-93.043&detailLat=41.902&detailLon=-93.208&width=650&height=450&zoom=10&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=mph&metricTemp=default&radarRange=-1"></iframe>
+      <iframe width="487.5" height="337.5" src="https://embed.windy.com/embed2.html?lat=44.949&lon=-93.043&detailLat=41.902&detailLon=-93.208&width=650&height=450&zoom=10&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=mph&metricTemp=default&radarRange=-1"></iframe>
     </div>
   );
 }
