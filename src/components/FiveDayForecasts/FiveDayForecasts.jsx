60589c705a6f42e8edb20a93414d68d054a03135
MarkTerry3 <mark.terry93@gmail.com>
2022-03-21 12:54:44 -0500

    Going to try bootstrap instead of mui

--- src/components/FiveDayForecasts/FiveDayForecasts.jsx
+++ src/components/FiveDayForecasts/FiveDayForecasts.jsx
@@ -13,37 +13,67 @@ function FiveDayForecast({ day }) {
     const handleChange = (event) => {
         setSpacing(Number(event.target.value));
     };
+    
 
     const jsx = `
 <Grid container spacing={${spacing}}>
 `;
 
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+    // return (
+    //     <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+    //         <Grid item xs={12}>
+    //             <Grid container justifyContent="center" spacing={spacing}>
+    //                 {[0].map((value) => (
+    //                     <Grid key={value} item>
+    //                         <Paper
+    //                             sx={{
+    //                                 height: 140,
+    //                                 width: 100,
+    //                                 color: 'white',
+    //                                 backgroundColor: '#1A2027'
+    //                                     // (theme) =>
+    //                                     //     theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    //                             }}
+    //                         >
+    //                             {day.Date.substring(0, 10)}
+    //                             Hi: {day.Temperature.Maximum.Value}
+    //                             Lo: {day.Temperature.Minimum.Value}
+
+    //                         </Paper>
+    //                     </Grid>
+    //                 ))}
+    //             </Grid>
+    //         </Grid>
+    //     </Grid>
+    // );
+
     return (
-        <Grid sx={{ flexGrow: 1 }} container spacing={2}>
-            <Grid item xs={12}>
-                <Grid container justifyContent="center" spacing={spacing}>
-                    {[0].map((value) => (
-                        <Grid key={value} item>
-                            <Paper
-                                sx={{
-                                    height: 140,
-                                    width: 100,
-                                    backgroundColor: (theme) =>
-                                        theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-                                }}
-                            >
-                                {day.Date}
-                                Hi: {day.Temperature.Maximum.Value}
-                                Lo: {day.Temperature.Minimum.Value}
-
-
-                            </Paper>
-                        </Grid>
-                    ))}
-                </Grid>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Item>xs=8</Item>
             </Grid>
-        </Grid>
-    );
+            <Grid item xs={4}>
+              <Item>xs=4</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item>xs=4</Item>
+            </Grid>
+            <Grid item xs={8}>
+              <Item>xs=8</Item>
+            </Grid>
+          </Grid>
+        </Box>
+      );
 }
 
 
