cf32f146f0a9e99da530c7af9e07674172fde86c
MarkTerry3 <mark.terry93@gmail.com>
2022-03-23 09:27:58 -0500

    react-leaflet installed

--- src/components/RadarView/RadarView.jsx
+++ src/components/RadarView/RadarView.jsx
@@ -15,16 +15,31 @@ function RadarView () {
         history.push('/home');
     }
 
+// import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
-
-    return(
-        <>
-        <button onClick={goBack}>Go back</button>
-        <Grid container spacing={2} sx={{ width: '70%', height: '350px', margin: 'auto' }}>
+    // return(
+    //     <>
+    //     <button onClick={goBack}>Go back</button>
+    //     <Grid container spacing={2} sx={{ width: '70%', height: '350px', margin: 'auto' }}>
             
-        </Grid>
-        </>
-    )
+    //     </Grid>
+    //     </>
+    // )
+
+
+    render(
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )
 }
 
 
