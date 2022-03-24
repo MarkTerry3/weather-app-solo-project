ccfa32ce2ed928c6764be818b47df17aa05be8ae
MarkTerry3 <mark.terry93@gmail.com>
2022-03-24 15:37:56 -0500

    if state written for weatherText, not appending pictures correctly... yet

--- src/components/FiveDayForecasts/FiveDayForecasts.jsx
+++ src/components/FiveDayForecasts/FiveDayForecasts.jsx
@@ -5,8 +5,11 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import snowImage from '../../weatherTextImages/rain-snow.png'
-
-
+import { useSelector } from 'react-redux';
+import '../../weatherTextImages/sunny.png';
+import '../../weatherTextImages/cloudy.png';
+import '../../weatherTextImages/mostly-cloudy.png';
+import '../../weatherTextImages/tstorms.png';
 
 
 
@@ -28,6 +31,8 @@ function FiveDayForecast({ day }) {
 
 
     // if statement for weatherText Images
+
+
 
 
     const theDate = new Date(day.Date);
@@ -66,16 +71,38 @@ function FiveDayForecast({ day }) {
 
     // );
 
- 
 
+    // src={imageFunction(day.Day.IconPhrase)}
+    // weatherImage(day.Day.IconPhrase)
+
+    function weatherImage(text) {
+        // 4 ifs
+        if (text === 'Sunny' || 'Mostly Sunny' || 'Partly Sunny' || 'Hazy Sunshine' || 'Partly Sunny w/ Flurries' || 'Clear' || 'Mostly Clear') { //sunny
+            text = '../../weatherTextImages/sunny.png';
+            return text;
+        } if (text === 'Intermittent Clouds' || 'Mostly Cloudy' || 'Cloudy' || 'Dreary (Overcast)' || 'Flurries' || 'Mostly Cloudy w/ Flurries' || 'Snow' || 'Freezing Rain') {     //cloudy
+            text = '../../weatherTextImages/cloudy.png';
+            return text;
+        } if (text === 'Fog' || 'Mostly Cloudy w/ Snow' || 'Partly Cloudy' || 'Intermittent Clouds') {  //partly cloudy
+            text = '../../weatherTextImages/mostly-cloudy.png';
+            return text;
+        } if (text === 'Showers' || 'Mostly Cloudy w/ Showers' || 'Partly Sunny w/ Showers' || 'T-Storms' || 'Mostly Cloudy w/ T-Storms' || 'Partly Sunny w/ T-Storms' || 'Rain' || 'Ice' || 'Sleet' || 'Rain and Snow' ||
+            'Partly Cloudy w/ Showers' || 'Mostly Cloudy w/ Showers' || 'Partly Cloudy w/ T-Storms' || 'Mostly Cloudy w/ T-Storms' || 'Mostly Cloudy w/ Flurries' || 'Mostly Cloudy w/ Snow') { //rainy
+            text = '../../weatherTextImages/tstorms.png';
+            return text;
+        } else return '../../weatherTextImages/rain-snow.png';
+
+
+
+    }
 
     return (
 
-                <Grid item xs={6} lg={2.4} sx={{textAlign: 'center'}}>
-                    <Paper sx={{py: 3, color: 'white', backgroundColor: '#37485c'}}>
-                    <Typography>{daysOfWeek[theFormattedDate]} <br/>Hi {day.Temperature.Maximum.Value} <br/> <img width="100px" src={snowImage}/> <br/> Lo {day.Temperature.Minimum.Value}</Typography>
-                    </Paper>
-                </Grid>
+        <Grid item xs={6} lg={2.4} sx={{ textAlign: 'center' }}>
+            <Paper sx={{ py: 3, color: 'white', backgroundColor: '#37485c' }}>
+                <Typography>{daysOfWeek[theFormattedDate]} <br />Hi {day.Temperature.Maximum.Value} <br /> <img width="100px" src={weatherImage(day.Day.IconPhrase)} /> <br /> Lo {day.Temperature.Minimum.Value}</Typography>
+            </Paper>
+        </Grid>
 
     );
 
